496e6d2d792075a08d835df38d63bd77f788e4d6
c-kel <christopher.kellie@my.jcu.edu.au>
2017-03-16 19:07:30 +1000

    number formats

---
+++ math_exm_02/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//import "fmt"
+
+func main() {
+	for i := 0
+	i < 200; i++{
+	//	fmt.Printf("%d - %b \n", 42, 42)
+		fmt.Printf("0d%d \t 0b%b \t 0x%x \n", 42, 42, 42)
+	}
+}
+
+
---
+++ math_exm_02/mathexm.go
@@ -0,0 +1,9 @@
+package helloworld_01
+
+import "fmt"
+
+func main() {
+	PrintLn("%d - %b", 42 , 42)
+}
+
+
